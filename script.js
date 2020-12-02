fb750111f52bba67d59e13f10367988c84d09368
heuzin <matheusqs.arq@gmail.com>
2020-12-02 11:10:18 -0500

    adding image, title and sources to the DOM

--- script.js
+++ script.js
@@ -1,3 +1,46 @@
+const imageContainer = document.getElementById('image-container');
+const loader = document.getElementById('loader')
+
+let photosArray = [];
+
+// Heler Function to Set Attributes on DOM elements
+function setAttributes(element, attributes) {
+    for (const key in attributes) {
+        element.setAttribute(key, attributes[key])
+    }
+}
+
+// Create Elements for Links & Photos, Add to DOM
+function displayPhotos() {
+    // Run Function for each object in photosArray
+    photosArray.forEach((photo) => {
+        // Crate <a> to link to Unsplash
+        const item = document.createElement('a')
+        // item.setAttribute('href', photo.links.html)
+        // item.setAttribute('target', '_blank');
+        setAttributes(item, {
+            href: photo.links.html,
+            target: '_blank',
+
+        });
+
+        // create <img> from photo
+        const img = document.createElement('img')
+        // img.setAttribute('src', photo.urls.regular)
+        // img.setAttribute('alt', photo.alt_description)
+        // img.setAttribute('title', photo.alt_description)
+        setAttributes(img, {
+            src: photo.urls.regular,
+            alt: photo.alt_description,
+            title: photo.alt_description
+        })
+
+        // Put <img> inside <a>, then put both inside imageContainer element
+        item.appendChild(img);
+        imageContainer.appendChild(item)
+    });
+}
+
 // Unsplash API
 const count = 10;
 const apiKey = 'AHMrutW5UDKqw0WlKA11T2TMZT2CaJ_S_XOqsnndePE'
@@ -7,8 +50,10 @@ const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&coun
 async function getPhotos() {
     try {
         const response = await fetch(apiUrl)
-        const data = await response.json()
-        console.log(data)
+        photosArray = await response.json()
+        
+        console.log(photosArray)
+        displayPhotos()
     } catch (error) {
         // Catch Error here
     }
